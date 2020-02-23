c4b068ca3c4dda1b2432db1bd46d0e0b7207ca33
Sheledons <51431377+Sheledons@users.noreply.github.com>
2020-02-23 20:52:15 +0800

    do

---
+++ src/dao/UserDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import domain.User;
+
+public class UserDao implements UserDaoInter {
+
+	public User verUser(User u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

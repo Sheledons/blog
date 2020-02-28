7446503aa8848c9606163daeb6049754891b5245
Sheledons <51431377+Sheledons@users.noreply.github.com>
2020-02-28 21:43:17 +0800

    2-28

---
+++ src/servlet/SignOutServlet.java
@@ -0,0 +1,36 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SignOutServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session=request.getSession();
+		session.invalidate();
+		boolean flag=true;
+		ObjectMapper mapper=new ObjectMapper();
+		response.setContentType("application/json;charset=utf-8");
+		mapper.writeValue(response.getOutputStream(),flag);
+	}
+
+}
